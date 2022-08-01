8b8b682f08ec2c4de0fcb4399c66ffdabef22c41
Chinomso Nwachukwu <noranwachukwu@gmail.com>
2022-08-01 17:16:23 +0100

    saving

---
+++ src/HemisphereDisplay.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const HemisphereDisplay = ()=>{
+    return(
+        <div> you are in northern hemisphere</div>
+    );
+};
+
+export default HemisphereDisplay
--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+/*const App = ()=> {
+            navigator.geolocation.getCurrentPosition((position => {console.log(position)}),
+                (error=>{console.log(error)}));
+}*/
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {latitude: null, errorMessage: ''}
+
+        navigator.geolocation.getCurrentPosition((position) => {
+                this.setState({latitude: position.coords.latitude})
+            },
+            (error) => {
+                this.setState({errorMessage: error.message})
+            }
+        )
+    }
+
+    render() {
+        if (this.state.errorMessage && !this.state.latitude) {
+            return (
+                <div> {this.state.errorMessage}</div>)
+        }
+        if (!this.state.errorMessage && this.state.latitude) {
+            return (
+                <div>{this.state.latitude}</div>
+            )
+        }
+
+        //else
+            {
+            //    return (<div>loading...</div>)
+            }//
+        }
+
+    }
+
+ReactDOM.render(
+    <App/>,
+    document.querySelector('#root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
