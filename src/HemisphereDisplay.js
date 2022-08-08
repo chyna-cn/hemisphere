c9cbb32b4b595c8757055e836f2b52dc85ea14aa
Chinomso Nwachukwu <noranwachukwu@gmail.com>
2022-08-08 17:43:27 +0100

    saving

--- src/HemisphereDisplay.js
+++ src/HemisphereDisplay.js
@@ -3,7 +3,7 @@ import northernPic from './image/northern hemisphere.jpg';
 import southernPic from './image/southern hemisphere.jpg';
 const HemisphereDisplay = ({latitude})=>{
     const hemisphere = latitude > 0 ? 'northern hemisphere': 'southern hemisphere'
-    const picture = latitude > 0 ? 'northernPic' : 'southernPic'
+    const picture = latitude > 0 ? northernPic : southernPic
     return(
         <div>
             <img src = {picture} alt='globe'/>
