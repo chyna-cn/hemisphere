c0a2d2ee29432d9982df414c746ee4612cc23ebe
Chinomso Nwachukwu <noranwachukwu@gmail.com>
2022-08-10 18:09:57 +0100

    saving

--- src/HemisphereDisplay.js
+++ src/HemisphereDisplay.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import './Hemisphere.css';
 import northernPic from './image/northern hemisphere.jpg';
 import southernPic from './image/southern hemisphere.jpg';
+
+const HemisphereConfig = {
+    southern:{
+        text:'this is southern hemisphere',
+        picture:southernPic
+    },
+    northern:{
+        text: 'this is northern hemisphere',
+        picture: northernPic
+    }
+}
+
 const HemisphereDisplay = ({latitude})=>{
-    const hemisphere = latitude > 0 ? 'northern hemisphere': 'southern hemisphere'
-    const picture = latitude > 0 ? northernPic : southernPic
+    const hemisphere = latitude > 0 ? 'northern': 'southern'
+    const {text,picture} = HemisphereConfig[hemisphere]
+
     return(
-        <div>
-            <img src = {picture} alt='globe'/>
-            {hemisphere}
+        <div className={hemisphere}>
+            <div className='ui raised text container segment'>
+                <div className='image'><img src = {picture} alt='globe picture'/></div>
+                <div className='ui teal bottom attached label'><h1>{text}</h1></div>
+            </div>
         </div>
     );
 };
